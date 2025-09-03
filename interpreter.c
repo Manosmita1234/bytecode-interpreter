a1a55422314541b5fa20e868367af0442141aa4e
Manosmita Sahu <manosmita.sahu.05@gmail.com>
2025-09-03 23:02:12 +0530

    added swap and duplicate operations

--- interpreter.c
+++ interpreter.c
@@ -9,7 +9,12 @@ int sp = -1;
 enum opcode {
     OP_PUSH,
     OP_ADD,
+    OP_SUBTRACTION,
+    OP_MULTIPLY,
+    OP_DIVISION,
     OP_PRINT,
+    OP_DUPLICATE,
+    OP_SWAP
 };
 
 void push(int value){
@@ -31,11 +36,12 @@ int pop(){
         
 }
 
+
 int main(){
     int program[]={
         OP_PUSH,5,
-        OP_PUSH,3,
-        OP_ADD,
+        OP_PUSH,10,
+        OP_SWAP,OP_SUBTRACTION,
         OP_PRINT
     };
 
@@ -60,6 +66,46 @@ int main(){
             case OP_PRINT:{
                 int value = pop();
                 printf("%d\n", value);
+                break;
+            }
+
+            case OP_SUBTRACTION : {
+                int b = pop();
+                int a = pop();
+                push(a-b);
+                break;
+            }
+
+            case OP_MULTIPLY : {
+                int b = pop();
+                int a = pop();
+                push(a*b);
+                break;
+            }
+
+            case OP_DIVISION : {
+                int b = pop();
+                int a = pop();
+                push(a/b);
+                break;
+            }
+
+            case OP_DUPLICATE : {
+                if(sp>=0){
+                    int value = stack[sp];
+                    push(value);
+                }
+                else{
+                    printf("stack is empty");
+                }
+            }
+
+            case OP_SWAP : {
+                int topValue = stack[sp];
+                int bottomValue = stack[sp-1];
+                stack[sp-1] = topValue;
+                stack[sp] = bottomValue;
+                break;
             }
         }
     }
